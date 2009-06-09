5565736b553752626c691a6a0186f3d2f92449db
Alexandre Point <apoint@users.sourceforge.net>
2009-06-09 16:23:04 +0000

    Fixed a typo in the bom/sources.mk file.

--- travel-ccm/travelccm/bom/sources.mk
+++ travel-ccm/travelccm/bom/sources.mk
@@ -5,7 +5,7 @@ bom_h_sources = $(top_srcdir)/travelccm/bom/BomAbstract.hpp \
 				$(top_srcdir)/travelccm/bom/TravelSolutionHolder.hpp \
 				$(top_srcdir)/travelccm/bom/Restriction.hpp \
 				$(top_srcdir)/travelccm/bom/Airline.hpp \
-				$(top_srcdir)/travelccm/bom/RestrictionList.hpp \ 
+				$(top_srcdir)/travelccm/bom/RestrictionList.hpp \
 				$(top_srcdir)/travelccm/bom/CCM.hpp \
 				$(top_srcdir)/travelccm/bom/TravelSolution.hpp \
 				$(top_srcdir)/travelccm/bom/Request.hpp
