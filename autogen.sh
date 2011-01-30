401429bdcea3e0ac2c8e36db3c3880dbd164ca61
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-01-30 18:54:09 +0100

    [TravelCCM][Test] Migrated to Boost Unit Test Framework (UTF), from ExtraCC/CPPUnit. Get rid from redundant types and exceptions.

--- autogen.sh
+++ autogen.sh
@@ -35,7 +35,7 @@ echo "- autoreconf." && autoreconf -fvi	&& \
 echo "" && \
 echo "Now, the configuration script has to be run. For instance:" && \
 echo "mkdir -p tmp && cd tmp && ../configure --prefix=/home/user/dev/deliveries/travelccm-99.99.99 \\" && \
-echo " --with-extracc=/home/user/dev/deliveries/extracc-stable --with-stdair=/home/user/dev/deliveries/stdair-stable --srcdir=.." && \
+echo " --with-stdair=/home/user/dev/deliveries/stdair-stable --srcdir=.." && \
 echo "" && exit 0
 
 #echo "- configure."            && ./configure "$@"             && exit 0
