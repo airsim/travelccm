c23ad38034348fff2c074bf1f4fadaa70abda20e
Anh Quan Nguyen <quan.nguyen@amadeus.com>
2011-03-09 10:47:14 +0100

    [travelcmm] Take into account the avl in the price-oriented model.

--- travelccm/bom/PriceOrientedModel.cpp
+++ travelccm/bom/PriceOrientedModel.cpp
@@ -36,6 +36,33 @@ namespace TRAVELCCM {
              lFareOptionList.begin(); itFO != lFareOptionList.end(); ++itFO) {
         const stdair::FareOptionStruct& lCurrentFO = *itFO;
 
+        // Check the availability.
+        const stdair::NbOfSeats_T& lPartySize = iBookingRequest.getPartySize();
+        const stdair::ClassList_StringList_T& lClassPath =
+          lCurrentFO.getClassPath();
+        const stdair::ClassAvailabilityMapHolder_T& lClassAvailabilityMapHolder =
+          lCurrentTS.getClassAvailabilityMapHolder();
+        bool lAvlSuff = true;
+        stdair::ClassAvailabilityMapHolder_T::const_iterator itCAMH =
+          lClassAvailabilityMapHolder.begin();
+        stdair::ClassList_StringList_T::const_iterator itClassList =
+          lClassPath.begin();
+
+        for (; itCAMH != lClassAvailabilityMapHolder.end(),
+               itClassList != lClassPath.end(); ++itCAMH, ++itClassList) {
+          const stdair::ClassList_String_T& lCurrentClassList = *itClassList;
+          const stdair::ClassAvailabilityMap_T& lClassAvlMap = *itCAMH;
+          stdair::ClassCode_T lFirstClass;
+          lFirstClass.append (lCurrentClassList, 0, 1);
+          const stdair::ClassAvailabilityMap_T::const_iterator itClassAvl =
+            lClassAvlMap.find (lFirstClass);
+          assert (itClassAvl != lClassAvlMap.end());
+          const stdair::Availability_T& lAvl = itClassAvl->second;
+          if (lAvl < lPartySize) {
+            lAvlSuff = false;
+          }
+        }
+
         // Choose the current fare option and the current solution
         // if the current fare is lower than the current lowest fare.
         const stdair::Fare_T& lCurrentFare = lCurrentFO.getFare();
