56a51f8d7d431f736f416a86c4378d993d5d29d2
Anh Quan Nguyen <quan.nguyen@amadeus.com>
2011-12-12 16:28:25 +0100

    [dev] Added the hard restriction model.

--- travelccm/command/ChoiceManager.cpp
+++ travelccm/command/ChoiceManager.cpp
@@ -7,7 +7,7 @@
 #include <stdair/bom/TravelSolutionStruct.hpp>
 #include <stdair/bom/BookingRequestStruct.hpp>
 // TravelCCM
-#include <travelccm/bom/PriceOrientedModel.hpp>
+#include <travelccm/bom/HardRestrictionModel.hpp>
 #include <travelccm/command/ChoiceManager.hpp>
 
 namespace TRAVELCCM {
@@ -16,9 +16,10 @@ namespace TRAVELCCM {
   const stdair::TravelSolutionStruct* ChoiceManager::
   chooseTravelSolution (stdair::TravelSolutionList_T& ioTravelSolutionList,
                         const stdair::BookingRequestStruct& iBookingRequest) {
-    // Return the cheapest solution.
+    // Return the cheapest solution which satisfies the hard restrictions
+    // such as change fees and non refundable.
     const stdair::TravelSolutionStruct* oTravelSolution_ptr = 
-      PriceOrientedModel::chooseTravelSolution (ioTravelSolutionList,
+      HardRestrictionModel::chooseTravelSolution (ioTravelSolutionList,
                                                 iBookingRequest);
     return oTravelSolution_ptr;
   }
